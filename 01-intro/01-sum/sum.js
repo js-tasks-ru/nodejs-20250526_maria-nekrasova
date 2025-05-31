49463b6cd21d66e9022c7979c2489a235fd5973a
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-05-31 16:23:10 +0300

    Added task sum

---
+++ 01-intro/01-sum/sum.js
@@ -0,0 +1,6 @@
+export default function sum(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        throw new TypeError("Both arguments must be numbers");
+    }
+    return a + b;
+}
